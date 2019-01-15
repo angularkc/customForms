edfb1b6dad88a36aa564341d8196fa83c3a3dacd
Steven Barnhurst <sbarnhurst@ringmastertech.com>
2019-01-15 09:15:00 -0600

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit {
+  form: FormGroup;
+  formValue: any = {};
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      ageMin: new FormControl(null, [Validators.min(0), Validators.max(130)]),
+      ageMax: new FormControl(null, [Validators.min(0), Validators.max(130)])
+    });
+  }
+
+  submit() {
+    this.formValue = this.form.getRawValue();
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
